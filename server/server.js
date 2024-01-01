7bdee3ff762f4c146c4959b99579c6dfae21748f
Vivek Mobiuso <vivek@mobiuso.com>
2024-01-01 15:41:23 +0530

    Production ready

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes.js");
 const chatRoutes = require("./routes/chatRoutes.js");
 const messageRoutes = require("./routes/messageRoutes.js");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
+const path = require("path")
 
 dotenv.config();
 
@@ -12,24 +13,39 @@ connectDB();
 const app = express()
 app.use(express.json())
 
-app.get("/abc", (req, res) => {
-    res.json({ msg: "Api is running" });
-})
 
 app.use("/api/user", userRoutes)
 app.use("/api/chat", chatRoutes)
 app.use("/api/message", messageRoutes)
 
+// ---------------- Deployment --------------------------
+
+const __dirname1 = path.resolve()
+console.log(__dirname1);
+if (process.env.NODE_ENV === 'production') {
+    const parentDir = path.join(__dirname1, '..');
+    app.use(express.static(path.join(parentDir, '/client/build')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(parentDir,"client","build","index.html"))
+    })
+} else {
+    app.get("/abc", (req, res) => {
+        res.json({ msg: "Api is running" });
+    })
+}
 app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server started on port ${PORT}`))
-
+// const parentDir = path.join(__dirname1, '..');
 const io = require("socket.io")(server, {
+    
     pingTimeout: 60000,
     cors: {
         origin: "http://localhost:3000",
+        // origin: `${parentDir}/client/build/index.html`,
     }
 })
 
@@ -53,7 +69,7 @@ io.on("connection", (socket) => {
     socket.on("new message", (newMessageRecieved) => {
         var chat = newMessageRecieved.chat
 
-        if (!chat.users) return console.log("users.chat not defined");
+        if (!chat.users) return console.log("chat.users not defined");
 
         // If a group message is recieved then it should be only recieved by other peoples other than the sender
         chat.users.forEach((user) => {
